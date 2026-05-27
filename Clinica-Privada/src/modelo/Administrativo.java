45a718a1233014edfe2b22040d004221cc93155b
MarjorieValdivieso <marjorievillacis430@gmail.com>
2026-05-26 20:17:58 -0500

    clase administrativo

--- Clinica-Privada/src/modelo/Administrativo.java
+++ Clinica-Privada/src/modelo/Administrativo.java
@@ -3,5 +3,54 @@ package modelo;
 public class Administrativo extends Empleado{
     private String departamento;
     private int horasTrabajadas;
-    private double valor
+    private double valorHora;
+
+    public Administrativo(String cedula, String nombre, int edad, String telefono, String correo, String departamento, int horasTrabajadas, double valorHora) {
+        super(cedula, nombre, edad, telefono, correo);
+        this.departamento = departamento;
+        this.horasTrabajadas = horasTrabajadas;
+        this.valorHora = valorHora;
+    }
+
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    public double getValorHora() {
+        return valorHora;
+    }
+
+    public int getHorasTrabajadas() {
+        return horasTrabajadas;
+    }
+
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+
+    public void setHorasTrabajadas(int horasTrabajadas) {
+        this.horasTrabajadas = horasTrabajadas;
+    }
+
+    public void setValorHora(double valorHora) {
+        this.valorHora = valorHora;
+    }
+
+    public Administrativo(String cedula, String nombre, int edad, String telefono, String correo) {
+        super(cedula, nombre, edad, telefono, correo);
+    }
+
+    @Override
+    public double calcularPago() {
+        return horasTrabajadas*valorHora;
+    }
+
+    @Override
+    public void mostrarInformacion() {
+        super.mostrarInformacion();
+        System.out.println("Departamento: " + departamento);
+        System.out.println("Horas trabajadas: " + horasTrabajadas);
+        System.out.println("Valor por hora: " + valorHora);
+        System.out.println("Pago: " + calcularPago());
+    }
 }
