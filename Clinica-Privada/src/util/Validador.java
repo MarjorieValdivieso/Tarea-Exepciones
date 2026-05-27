2249cc0bf475db1a7363d6fb9e6f29b5e6fdde7f
MarjorieValdivieso <marjorievillacis430@gmail.com>
2026-05-26 20:21:31 -0500

    clase Validador

--- Clinica-Privada/src/util/Validador.java
+++ Clinica-Privada/src/util/Validador.java
@@ -1,4 +1,22 @@
 package util;
 
 public class Validador {
+    public boolean validarCorreo(String correo){
+        return correo.contains("@") && correo.contains(".");
+    }
+    public boolean validarTelefono(String telefono) {
+
+        for (int i = 0; i < telefono.length(); i++) {
+
+            if (!Character.isDigit(telefono.charAt(i))) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public boolean campoVacio(String texto) {
+        return texto.trim().isEmpty();
+    }
 }
